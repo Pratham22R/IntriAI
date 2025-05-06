84733136ef0be92e9664a747aa7cec7c51b17a1e
Sambhav Raghuvanshi <pratham2262003@gmail.com>
2025-05-06 12:10:26 +0530

    minor fixes

--- app/dashboard/create-new/_components/RoomType.jsx
+++ app/dashboard/create-new/_components/RoomType.jsx
@@ -12,10 +12,10 @@ function RoomType({ selectedRoomType }) {
     <div className="w-full">
       <label htmlFor="" className="text-slate-400 mb-2 block">Select Room Type*</label>
       <Select onValueChange={(value)=>selectedRoomType(value)}>
-        <SelectTrigger className="w-full">
+        <SelectTrigger className="w-full text-black">
           <SelectValue placeholder="Room Type" />
-        </SelectTrigger>
-        <SelectContent className="w-full">
+        </SelectTrigger> 
+        <SelectContent className="w-full ">
           <SelectItem value="Bedroom">Bedroom</SelectItem>
           <SelectItem value="Bathroom">Bathroom</SelectItem>
           <SelectItem value="Kitchen">Kitchen</SelectItem>
